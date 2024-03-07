e6474ab2a0c5681cfb877dcf59dd53221a98c170
MACIA Maxence <maxence.macia@infotel.com>
2024-03-07 14:41:00 +0100

    Project initilisation

---
+++ src/main/java/com/maxencemacia/collectionSaver/CollectionSaverApplication.java
@@ -0,0 +1,13 @@
+package com.maxencemacia.collectionSaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionSaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxencemacia/collectionSaver/CollectionSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxencemacia.collectionSaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
